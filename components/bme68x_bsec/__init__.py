26e5a78b393ff12a50f9e534d1764427e0030123
Clément FAYARD <clementfayard@hotmail.com>
2023-08-20 14:25:22 -0700

    Add esp32 support

--- components/bme68x_bsec/__init__.py
+++ components/bme68x_bsec/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c
+from esphome.components import i2c, esp32
 from esphome.const import CONF_ID
 
 CODEOWNERS = ["@lildinosaur"]
